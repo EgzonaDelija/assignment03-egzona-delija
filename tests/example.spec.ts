b90660ee9bda37b272f7bdb1c064dc85f2f317dc
”Egzona <Egze_14@hotmail.com>
2024-10-07 13:59:24 +0200

    pipeline update

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,18 +5,21 @@ test.describe('Front-end tests', () => {
 
   test('Create a client', async ({ page }) => {
 
-    await page.goto('http://localhost:3000');
+    await page.goto('http://localhost:3000/login/');
+    await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
     await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
     await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
     await page.getByRole('button', { name: 'Login' }).click();
-    await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
+    await page.waitForTimeout(10000);
+    await expect(page.getByRole('heading', { name: 'Tester Hotel Overview'})).toBeVisible();
+    await page.locator('div').filter({ hasText: /^ClientsNumber: 2View$/ }).getByRole('link').click();
     await page.getByRole('link', { name: 'Create Client' }).click();
     await expect(page.getByRole('heading', { name: 'New Client'})).toBeVisible();
-    await page.fill('#app > div > div:nth-child(2) > div:nth-child(1) > input[type=text]', "Egzona Delija");
-    await page.fill('#app > div > div:nth-child(2) > div:nth-child(2) > input[type=email]', "Egzona.delija@gmail.se");
-    await page.fill('#app > div > div:nth-child(2) > div:nth-child(3) > input[type=text]', "8744782646");
+    await page.fill('#app > div > div:nth-child(2) > div:nth-child(1) > input[type=text]', "Anna Johansson");
+    await page.fill('#app > div > div:nth-child(2) > div:nth-child(2) > input[type=email]', "Anna@HJohansson.se");
+    await page.fill('#app > div > div:nth-child(2) > div:nth-child(3) > input[type=text]', "28473268");
     await page.getByText('Save').click()
-    await expect(page.getByRole('heading', { name: 'Lena Handen'})).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Anna Johansson'})).toBeVisible();
   });
     
 
