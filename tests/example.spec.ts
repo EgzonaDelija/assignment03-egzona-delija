61ff0fce4bd5066230c2a8d76b870006964468c0
”Egzona <Egze_14@hotmail.com>
2024-10-07 18:19:41 +0200

    pipeline update

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -24,28 +24,41 @@ test.describe('Front-end tests', () => {
 
  /* test('Create Room', async ({ page }) => {
     
-    await page.goto('http://localhost:3000');
-    await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
-    await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
-    await page.getByRole('button', { name: 'Login' }).click();
-    await page.locator('#app > div > div > div:nth-child(1) > a').click();
-    await expect(page.getByRole('heading', { name: 'Rooms'})).toBeVisible();
-    await page.getByRole('link', { name: 'Create Room' }).click();
-    await page.getByRole('combobox').click();
-    await page.selectOption('#app > div > div:nth-child(2) > div:nth-child(1) > select', { label: 'Single' });
-    await page.fill('#app > div > div:nth-child(2) > div:nth-child(2) > input[type=number]', '3');
-    await page.fill('#app > div > div:nth-child(2) > div:nth-child(3) > input[type=number]', '3');
-    await page.locator('.checkbox').click();
-    await page.fill('#app > div > div:nth-child(2) > div:nth-child(5) > input[type=number]', '999');
-    await page.locator('#app > div > div:nth-child(2) > div:nth-child(6) > select > option:nth-child(3)').click();
-    await page.locator('#app > div > div.actions > a.btn.blue').click();
-    await expect(page.getByRole('heading', { name: 'Floor 3, Room' })).toBeVisible();
-    await expect(page.getByText('Category: single')).toBeVisible();
-    await expect(page.getByText('Price: 999kr')).toBeVisible();
-    await expect(page.getByText('Features: sea view')).toBeVisible();
-    await page.getByRole('button', { name: 'save' }).click()
+   
     
   });*/
   
 });
+
+test.describe('Backend tests', () => {
+
+  test('Create a client', async ({ request }) => {
+
+    const response = await request.post('http://localhost:3000/api/login', {
+
+      data:{
+
+        "username": `${process.env.TEST_USERNAME}`,
+
+        "password": `${process.env.TEST_PASSWORD}`
+
+      }      
+
+    });
+
+    expect (response.ok()).toBeTruthy();    
+
+    const createClientResponse = await request.post('http://localhost:3000/api/clients', {
+      data: {
+        "name": "Anna Johansson",
+        "email": "Anna@HJohansson.se",
+        "telephone": "28473268"
+      }
+    });
+
+
+  });  
+
+});
+
   
