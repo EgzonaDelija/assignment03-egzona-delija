678e1fb1a7d330ef88eea31408762f1137cbb748
”Egzona <Egze_14@hotmail.com>
2024-10-09 13:00:57 +0200

    pipeline update

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -15,22 +15,34 @@ test.describe('Front-end tests', () => {
     await page.locator('div').filter({ hasText: /^ClientsNumber: 2View$/ }).getByRole('link').click();
     await page.getByRole('link', { name: 'Create Client' }).click();
     await expect(page.getByRole('heading', { name: 'New Client'})).toBeVisible();
-    await page.fill('#app > div > div:nth-child(2) > div:nth-child(1) > input[type=text]', "Anna Johansson");
-    await page.fill('#app > div > div:nth-child(2) > div:nth-child(2) > input[type=email]', "Anna@HJohansson.se");
-    await page.fill('#app > div > div:nth-child(2) > div:nth-child(3) > input[type=text]', "28473268");
+    await page.fill('#app > div > div:nth-child(2) > div:nth-child(1) > input[type=text]', "Egzona Delija");
+    await page.fill('#app > div > div:nth-child(2) > div:nth-child(2) > input[type=email]', "Egzona@Delija.se");
+    await page.fill('#app > div > div:nth-child(2) > div:nth-child(3) > input[type=text]', "070200000");
     await page.getByText('Save').click()
     await expect(page.getByRole('heading', { name: 'Anna Johansson'})).toBeVisible();
   });
 
- /* test('Create Room', async ({ page }) => {
-    
-   
-    
-  });*/
-  
+  test('Log in and log out', async ({ page }) => {
+    await page.goto('http://localhost:3000/login/');
+    await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+
+    await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
+    await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.waitForTimeout(10000);
+    await expect(page.getByRole('heading', { name: 'Tester Hotel Overview'})).toBeVisible();
+
+    //Logga ut
+    await page.getByRole('button', { name: 'Logout' }).click();
+    await page.goto('http://localhost:3000');
+    await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+  });
+
+
+
 });
 
-  test('Test case 01 - Backend', async ({ request }) => {
+  test('Backend - Get all Clients', async ({ request }) => {
   const response = await request.post('http://localhost:3000/api/login', {
     headers: {
       'Content-Type': 'application/json',
@@ -61,7 +73,7 @@ test.describe('Front-end tests', () => {
 });
 
 
-test('Test case 02 - Backend', async ({ request }) => {
+test(' Backend - Get all Rooms', async ({ request }) => {
 
   const response = await request.post('http://localhost:3000/api/login', {
     headers: {
